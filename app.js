6b390be31f3a85a0a58bf55d9f6e70125ace49f3
Murphy Randle <murphyrandle@gmail.com>
2015-01-20 00:07:36 -0700

    Finished

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
 var app = angular.module('PupsApp', []);
 
-var controller = app.controller('MainController', function ($scope) {
-  $scope.helloMsg = "foo"
+app.config(function (animalProvider) {
+  animalProvider.setAnimal('bear')
+})
+
+app.value('apiBase', 'http://placekitten.com/')
+
+app.factory('kittenFactory', function(apiBase) {
+  return {
+    getKitten: function (w, h) {
+      console.log('Running kitten factory');
+      return apiBase + '/' + w + '/' + h;
+    }
+  }
+})
+
+app.service('kittenService', function (apiBase) {
+  this.getKitten = function (w, h) {
+    console.log('Running kitten service');
+    return apiBase + '/' + w + '/' + h;
+  }
+})
+
+app.provider('animal', function () {
+  var animal = 'kitten'
+  this.setAnimal = function (newAnimal) {
+    animal = newAnimal
+  }
+
+  function Animaller() {
+    this.getAnimal = function (w, h) {
+      console.log('Running animal provider');
+      return 'http://www.place' + animal + '.com/' + w + '/' + h;
+    }
+  }
+
+  this.$get = function () {
+    return new Animaller()
+  }
+})
+
+var controller = app.controller('MainController', function (
+    $scope, apiBase, kittenFactory, kittenService, animal) {
+  // $scope.hero = 'http://placekitten.com/800/500'
+  // $scope.hero = apiBase + '/800/500'
+  // $scope.hero = kittenFactory.getKitten(800, 500)
+  // $scope.hero = kittenService.getKitten(800, 500)
+  $scope.hero = animal.getAnimal(800, 500)
 })
